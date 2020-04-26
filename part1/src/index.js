98bf2ccafcec990da729bfb7ff09f782508243c6
Dennis Grabowski <dennisgrabowski@gmx.de>
2020-04-26 16:25:24 +0200

    part1: component: hello: introduce age parameter

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom'
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () => {
+  const baseAge = 1
+
   return (
     <div>
       <h1>Greetings!</h1>
-      <Hello name="Dennis"></Hello>
-      <Hello name="Denise"></Hello>
-      <Hello name="Deniz"></Hello>
+      <Hello name="Dennis" age={13 * 2 + baseAge * 2}></Hello>
     </div>
   )
 }
